ec7d89470736cbedd68c790b5bbbb605d990139e
Yaron <y.lambers@outlook.com>
2015-11-12 11:00:44 +0100

    added fontpack

--- includes/home.php
+++ includes/home.php
@@ -2,13 +2,21 @@
 <head>
     <title>Alfa workshops</title>
     <link rel="stylesheet" type="text/css" href="../styles/index.css">
+    <link rel="stylesheet" type="text/css" href="../styles/base.css"
 </head>
 <body>
     <div class="header">
         <img src="../img/logo-alfa.png">
     </div>
     <div class="menu">
-
+        <ul>
+            <li>
+                Home
+            </li>
+            <li>
+                Evementen
+            </li>
+        </ul>
     </div>
 
     <section>
