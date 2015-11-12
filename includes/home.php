f425d1ad756d196636fcca9d029cdd1ec3a27fa2
Yaron <y.lambers@outlook.com>
2015-11-12 10:42:07 +0100

    added index.css  for home.php style

---
+++ includes/home.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<head>
+    <title>Alfa workshops</title>
+    <link rel="stylesheet" type="text/css" href="../styles/index.css">
+</head>
+<body>
+    <div class="header">
+        <img src="../img/logo-alfa.png">
+    </div>
+    <div class="menu">
+
+    </div>
+
+    <section>
+
+    </section>
+</body>
+
+</html>
