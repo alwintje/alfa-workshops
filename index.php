f9154fc1cd9b40b5c82e2b11bf77af431a4b4a82
alwintje <alwinkroesen@gmail.com>
2015-11-19 09:32:43 +0100

    workshops

--- index.php
+++ index.php
@@ -36,24 +36,43 @@ if(isset($_GET['logout'])){
             <li>
                 <a href="?">Evenementen</a>
             </li>
-            <li>
-                <a href="?logout">Loguit</a>
-            </li>
             <?php
-                if($user['role'] == 2){
-                    echo '
+            if($user['role'] == 2){
+                echo '
                         <li>
                             <a href="?addEvent">Evenementen toevoegen</a>
                         </li>
                     ';
-                }
+            }
             ?>
+            <li>
+                <a href="?logout">Loguit</a>
+            </li>
         </ul>
     </div>
     <section class="events">
         <?php
+        if(isset($_GET['editEvent'])){
+            if(isset($_GET['add'])){
 
-        if(isset($_GET['workshops'])){
+            }
+        ?>
+            <article class="text-box">
+                <form action="?editEvent&add">
+                    <label for="name">Naam:</label><input type="text" name="name" id="name" />
+                    <label for="description">Omschrijving:</label><input type="text" name="description" id="description" />
+                    <label for="date">Datum:</label><input type="text" name="date" id="date" />
+                    <label for="startdate_registration">Start aanmeldingen:</label><input type="text" name="startdate_registration" id="startdate_registration" />
+                    <label for="enddate_registration">Einde aanmeldingen:</label><input type="text" name="enddate_registration" id="enddate_registration" />
+                    <label for="rating">Waarderingen:</label><input type="text" name="rating" id="rating" />
+                    <label for="mail_confirm">Mail bevestiging:</label><input type="text" name="mail_confirm" id="mail_confirm" />
+                </form>
+            </article>
+
+        <?
+
+
+        }elseif(isset($_GET['workshops'])){
             $query = $db->doquery("SELECT * FROM {{table}} WHERE event='".$_GET['workshops']."' ","workshops");
             while($row = mysqli_fetch_array($query)){
                 ?>
